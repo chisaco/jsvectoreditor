b6c8e039dfdb5ed15a0cd8b939c3f5a5e0603b78
antimatter15 <antimatter15@8f9d902e-7703-11de-8480-e142f2bfdb3b>
2009-07-23 15:56:37 +0000

    more refined multi-select

--- trunk/editor.js
+++ trunk/editor.js
@@ -12,7 +12,6 @@ function VectorEditor(elem, width, height){
         return alert("Error! No Vector Graphics Library Found!"); //if renderer isn't there, return false;
     }
     
-    
     this.draw = Raphael(elem, width, height);
     
     this.onHitXY = [0,0]
@@ -28,7 +27,7 @@ function VectorEditor(elem, width, height){
     
     this.action = "";
     
-    
+    this.selectadd = false;
     
     this.shapes = []
     this.trackers = []
@@ -40,20 +39,66 @@ function VectorEditor(elem, width, height){
 }
 
 VectorEditor.prototype.setMode = function(mode){
-  this.unselect()
-  this.mode = mode;
-}
-
-VectorEditor.prototype.unselect = function(){
-  this.selected = [];
-  this.removeTracker();
-}
-
-VectorEditor.prototype.removeTracker = function(){
-  for(var i = 0; i < this.trackers.length; i++){
-    this.trackers[i].remove();
+  if(mode == "select+"){
+    this.mode = "select";
+    this.selectadd = true;
+    this.unselect()
+  }else if(mode == "select"){
+    this.mode = mode;
+    this.unselect()
+    this.selectadd = false;
+  }else if(mode == "delete"){
+    for(var i = 0; i < this.selected.length; i++){
+      this.deleteShape(this.selected[i])
+    }
+    this.selected = [];
+  }else{
+    this.unselect()
+    this.mode = mode;
   }
-  this.trackers = []
+}
+
+VectorEditor.prototype.unselect = function(shape){
+  if(!shape){
+    this.selected = [];
+    this.removeTracker();
+  }else{
+    this.array_remove(shape, this.selected);
+    for(var i = 0; i < this.trackers.length; i++){
+      if(this.trackers[i].shape == shape){
+        this.removeTracker(this.trackers[i]);
+      }
+    }
+  }
+}
+
+//from the vXJS JS Library
+VectorEditor.prototype.in_array = function(v,a){
+  for(var i=a.length;i--&&a[i]!=v;);
+  return i
+}
+
+//from vX JS, is it at all strange that I'm using my own work?
+VectorEditor.prototype.array_remove = function(e, o){
+  var x=this.in_array(e,o);
+  x!=-1?o.splice(x,1):0
+}
+
+VectorEditor.prototype.is_selected = function(shape){
+  return this.in_array(shape, this.selected) != -1;
+}
+
+VectorEditor.prototype.removeTracker = function(tracker){
+  if(!tracker){
+    for(var i = 0; i < this.trackers.length; i++){
+      this.removeTracker(this.trackers[i]);
+    }
+    this.trackers = []
+  }else{
+    try {
+      tracker.remove();
+    }catch(err){}
+  }
 }
 
 VectorEditor.prototype.moveTracker = function(x, y){
@@ -68,8 +113,19 @@ VectorEditor.prototype.moveTracker = function(x, y){
 }
 
 VectorEditor.prototype.selectAdd = function(shape){
-  this.selected.push(shape)
-  this.showGroupTracker(shape)
+  if(this.is_selected(shape) == false){
+    this.selected.push(shape)
+    this.showGroupTracker(shape)
+  }
+}
+
+VectorEditor.prototype.selectToggle = function(shape){
+  if(this.is_selected(shape) == false){
+    this.selected.push(shape)
+    this.showGroupTracker(shape)
+  }else{
+    this.unselect(shape)
+  }
 }
 
 VectorEditor.prototype.select = function(shape){
@@ -117,20 +173,21 @@ VectorEditor.prototype.onMouseDown = function(event){
   
   if(this.mode == "select" && !this.selectbox){
     if(target == this.draw.canvas){
-      this.unselect();
+      if(!this.selectadd) this.unselect();
       this.selectbox = this.draw.rect(x, y, 0, 0)
         .attr({"fill-opacity": 0.15, 
               "stroke-opacity": 0.5, 
               "fill": "#007fff",
               "stroke": "#007fff"});
     }else if(target.shape_object){
-      if(this.selected.length > 1){
+      if(this.selected.length > 1 || this.selectadd){
+        this.selectAdd(target.shape_object);
         this.action = "move";
       }else{
         this.select(target.shape_object);
         this.action = "move";
-        this.offsetXY = [target.shape_object.attr("x") - x,target.shape_object.attr("y") - y]
       }
+      this.offsetXY = [target.shape_object.attr("x") - x,target.shape_object.attr("y") - y]
     }
   }else if(this.selected.length == 0){
     var shape = null;
@@ -150,8 +207,10 @@ VectorEditor.prototype.onMouseDown = function(event){
       shape = this.draw.image("http://upload.wikimedia.org/wikipedia/commons/a/a5/ComplexSinInATimeAxe.gif", x, y, 0, 0);
     }else if(this.mode == "text"){
     }
-    shape.attr({fill: this.fill, stroke: this.stroke})
-    this.addShape(shape)
+    if(shape){
+      shape.attr({fill: this.fill, stroke: this.stroke})
+      this.addShape(shape)
+    }
   }else{
     if(this.mode == "polygon"){
         this.selected[0].lineTo(x, y)
@@ -189,7 +248,7 @@ VectorEditor.prototype.move = function(shape, x, y){
 
 VectorEditor.prototype.onMouseMove = function(event){
   var x = event.clientX,
-      y = event.clientY
+      y = event.clientY;
       
   if(this.mode == "select"){
     if(this.selectbox){
@@ -245,7 +304,7 @@ VectorEditor.prototype.showTracker = function(shape){
   if(shape.subtype == "line"){
     var line = Raphael.parsePathString(shape.attr('path'));
     var tracker = this.draw.set();
-    
+    tracker.shape = shape;
     tracker.push(this.trackerBox(line[0][1],line[0][2]))
     tracker.push(this.trackerBox(line[1][1],line[1][2]))
     
@@ -253,16 +312,18 @@ VectorEditor.prototype.showTracker = function(shape){
   }else if(shape.type == "rect" || shape.type == "image"){
     var tracker = this.draw.set();
     var box = shape.getBBox();
-    tracker.push(this.trackerBox(box.x, box.y))
-    tracker.push(this.trackerBox(box.x + box.width, box.y))
-    tracker.push(this.trackerBox(box.x + box.width, box.y + box.height))
-    tracker.push(this.trackerBox(box.x, box.y + box.height))
+    tracker.shape = shape;
+    tracker.push(this.draw.rect(box.x - 10, box.y - 10, box.width + 20, box.height + 20).attr({"opacity":0.3}))
+    tracker.push(this.trackerBox(box.x - 10, box.y - 10))
+    tracker.push(this.trackerBox(box.x + box.width + 10, box.y - 10))
+    tracker.push(this.trackerBox(box.x + box.width + 10, box.y + box.height + 10))
+    tracker.push(this.trackerBox(box.x - 10, box.y + box.height + 10))
     
     this.trackers.push(tracker)
   }else if(shape.type == "ellipse"){
-  
     var tracker = this.draw.set();
     var box = shape.getBBox();
+    tracker.shape = shape;
     tracker.push(this.trackerBox(box.x, box.y))
     tracker.push(this.trackerBox(box.x + box.width, box.y))
     tracker.push(this.trackerBox(box.x + box.width, box.y + box.height))
@@ -281,6 +342,7 @@ VectorEditor.prototype.showGroupTracker = function(shape){
     "stroke-dasharray": "-",
     "stroke": "blue"
   }))
+  tracker.shape = shape;
   this.trackers.push(tracker)
 }
 
@@ -291,43 +353,60 @@ VectorEditor.prototype.onDblClick = function(event){
   }
 }
 
+VectorEditor.prototype.deleteShape = function(shape){
+  if(shape && shape.node && shape.node.parentNode){
+    shape.remove()
+  }
+  for(var i = 0; i < this.trackers.length; i++){
+    if(this.trackers[i].shape == shape){
+      this.removeTracker(this.trackers[i]);
+    }
+  }
+  //should remove references, but whatever
+}
+
 VectorEditor.prototype.onMouseUp = function(event){
+  var target = event.target
   if(this.mode == "select"){
     if(this.selectbox){
       var sbox = this.selectbox.getBBox()
+      var new_selected = [];
       for(var i = 0; i < this.shapes.length; i++){
         if(this.rectsIntersect(this.shapes[i].getBBox(), sbox)){
-          this.selected.push(this.shapes[i])
-          //this.shapes[i].attr("fill","#007fff")
-          //this.showGroupTracker(this.shapes[i])
+          new_selected.push(this.shapes[i])
         }
       }
       
-      if(this.selected.length == 1){
-        this.showTracker(this.selected[0])
-      }else if(this.selected.length > 1){
-        for(var i = 0; i < this.selected.length; i++){
-          this.showGroupTracker(this.selected[i])
+      if(new_selected.length == 1){
+        this.select(this.selected[0])
+      }else if(new_selected.length > 1){
+        for(var i = 0; i < new_selected.length; i++){
+          this.selectAdd(new_selected[i])
         }
+      }else if(new_selected.length == 0){
+        this.unselect()
       }
       this.selectbox.remove()
       this.selectbox = null;
-      if(this.selected.length == 0){
-        this.unselect()
-      }
+      
     }else{
-      this.action = ""
+      this.action = "";
     }
-  }else if(this.mode == "rect"){
-    this.unselect()
-  }else if(this.mode == "ellipse"){
-    this.unselect()
-  }else if(this.mode == "path"){
-    this.unselect()
-  }else if(this.mode == "line"){
-    this.unselect()
-  }else if(this.mode == "image"){
-    this.unselect()
+  }else if(this.selected.length == 1){
+    if(this.selected[0].getBBox().height == 0 && this.selected[0].getBBox().width == 0){
+      this.deleteShape(this.selected[0])
+    }
+    if(this.mode == "rect"){
+      this.unselect()
+    }else if(this.mode == "ellipse"){
+      this.unselect()
+    }else if(this.mode == "path"){
+      this.unselect()
+    }else if(this.mode == "line"){
+      this.unselect()
+    }else if(this.mode == "image"){
+      this.unselect()
+    }
   }
 }
 
