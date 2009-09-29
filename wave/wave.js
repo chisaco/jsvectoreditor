f5b76b20790f182a1495df56f2880da4576bd165
antimatter15 <antimatter15@8f9d902e-7703-11de-8480-e142f2bfdb3b>
2009-09-29 21:58:57 +0000

    159

--- wave/wave.js
+++ wave/wave.js
@@ -131,7 +131,12 @@
         
         //console.log("newshape:",keys[i])
         //console.log("data",text)
-        
+        if(typeof text == "string" && text.indexOf("DEL/") == 0){
+          if((new Date()).getTime() - parseInt(text.substr(4)) < 5000){
+            //oh noes deleted
+            continue;
+          }
+        }
         var json = JSON.parse(text);
         
         if(editor.getShapeById(keys[i]) == null){
